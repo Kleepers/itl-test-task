80f34ce08fb4e190ac2e5468d78a6fc441226cfc
Vladislav <perezzzq@yandex.ru>
2024-03-03 21:22:47 +0300

    readme

--- src/pages/UserPostsPage/UserPostsPage.tsx
+++ src/pages/UserPostsPage/UserPostsPage.tsx
@@ -6,6 +6,10 @@ import {useEffect} from "react";
 
 export const UserPostsPage = () => {
 
+    // isError isLoading не делал, дизайна нет, в требованиях четко нет,
+    // в целом вставил бы это через useState и условный рендер
+    // + как вариант в zustand стейте это хранить
+
     const {userId} = useParams()
     const user = useUsersStore((state) => state.users.find((user) => user.id === Number(userId)))
 
