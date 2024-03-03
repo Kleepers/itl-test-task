b9f204e173f5b6d6daa65794e9135f5a5debbc9d
Vladislav <perezzzq@yandex.ru>
2024-03-03 18:30:35 +0300

    add color palette

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './vendor/fonts/fonts.scss'
+import './vendor/normalize.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
